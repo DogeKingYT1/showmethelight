0f170d4d734fde82c6b973bdade2c96dd4bdd8ff
Doge_KingYT1 <84642215+DogeKingYT1@users.noreply.github.com>
2026-01-02 02:11:20 -0500

    g

--- pages/admin.js
+++
@@ -1,8 +0,0 @@
-export default function Admin() {
-  return (
-    <main>
-      <h1>Admin Panel</h1>
-      <p>Protected admin panel</p>
-    </main>
-  );
-}
--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>showmethelight</h1>
-      <p>Uncovering truth and fact beyond smoke and mirrors.</p>
-    </main>
-  );
-}
